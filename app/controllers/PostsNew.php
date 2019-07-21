97822815a251eb15c0c43d65295be77f0ce09960
Anton <tony201994@mail.ru>
2019-07-22 01:12:39 +0400

    Add regexp to the Router.php, add controllers and actions #2

---
+++ app/controllers/PostsNew.php
@@ -0,0 +1,21 @@
+<?php
+
+
+class PostsNew {
+
+    public function indexAction() {
+        echo 'PostNew::index';
+    }
+    public function testAction() {
+        echo 'PostNew::test';
+    }
+
+    public function testPageAction() {
+        echo 'PostNew::testPage';
+    }
+
+    public function before() {
+        echo 'PostNew::before';
+    }
+
+}
