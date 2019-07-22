2acf8c7bd043eb04877272f83d1a0c66d417845a
Anton <tony201994@mail.ru>
2019-07-22 12:45:04 +0400

    Create base controller #6

--- vendor/core/Router.php
+++ vendor/core/Router.php
@@ -30,6 +30,7 @@ class Router {
                 if (!isset($route['action'])) {
                     $route['action'] = 'index';
                 }
+                $route['controller'] = self::upperCamelCase($route['controller']);
                 self::$route = $route;
                 return true;
             }
@@ -38,10 +39,11 @@ class Router {
     }
 
     public static function dispatch($url) {
+        $url = self::removeQueryString($url);
         if (self::matchRoute($url)) {
-            $controller = 'app\controllers\\' . self::upperCamelCase(self::$route['controller']);
+            $controller = 'app\controllers\\' . self::$route['controller'];
             if (class_exists($controller)) {
-               $cObj = new $controller;
+               $cObj = new $controller(self::$route);
                $action = self::lowerCamelCase(self::$route['action']) . 'Action';
                if (method_exists($cObj, $action)) {
                     $cObj->$action();
@@ -63,6 +65,17 @@ class Router {
 
     protected static function lowerCamelCase($name) {
         return lcfirst(self::upperCamelCase($name));
+    }
+
+    protected static function removeQueryString($url) {
+        if ($url) {
+            $params = explode('&', $url, 2);
+            if (false === strpos($params[0], '=')) {
+                return rtrim($params[0], '/');
+            } else {
+                return '';
+            }
+        }
     }
 
 }
